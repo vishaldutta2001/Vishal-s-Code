8039315b639fd05e95d77b1d180f3e081a15660b
Vishal Dutta <71959396+vishaldutta2001@users.noreply.github.com>
2023-05-04 16:20:58 +0530

    Time: 7 ms (89.71%), Space: 18.1 MB (90.37%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -4,19 +4,8 @@ public:
         int op=0;
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
-        int i=0;
-        while(i<seats.size()){
-            if(seats[i]==students[i]){
-                i++;
-            }
-            else if(seats[i]>students[i]){
-                students[i]+=1;
-                op++;
-            }
-            else{
-                students[i]-=1;
-                op++;
-            }
+        for(int i=0;i<seats.size();i++){
+            op+=abs(seats[i]-students[i]);
         }
         return op;
     }
