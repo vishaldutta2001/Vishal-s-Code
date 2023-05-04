5952fc064168f8b8c326d2ab6d12791b41c1f2a7
Vishal Dutta <71959396+vishaldutta2001@users.noreply.github.com>
2023-05-04 14:23:25 +0530

    Time: 5 ms (28.97%), Space: 6.5 MB (35.84%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
+        string a="";
         int cnt=0;
         for(int i=0;i<s.size();i++){
             if(s[i]==' '){
                 cnt++;
-                if(cnt==k){
-                    s.erase(i);
-                }
             }
+            if(cnt==k){
+                break;
+            }
+            a+=s[i];
+            
         }
-        return s;
+        return a;
     }
 };
