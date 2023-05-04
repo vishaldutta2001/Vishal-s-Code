4d04d20899e1b783385bfe8d39d0c3fa8e0f7e67
Vishal Dutta <71959396+vishaldutta2001@users.noreply.github.com>
2023-05-04 11:59:57 +0530

    Time: 3 ms (91.15%), Space: 8.5 MB (99.20%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -2,32 +2,14 @@ class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int cnt=0;
-        for(int i=0;i<nums.size()-2;i++){
-            bool firstCase=true;
-            bool secondCase=true;
-            int j=i+1;
-            for(;j<nums.size()-1;j++){
-                if(nums[j]-nums[i]==diff){
-                     break;
-                }
-                else if(j==nums.size()-2){
-                    firstCase=false;
-                }
-            }
-            if(firstCase){
-                for(int k=j+1;k<nums.size();k++){
-                    if(nums[k]-nums[j]==diff){
-                        break;
-                    }
-                    else if(k==nums.size()-1){
-                        secondCase=false;
-                    }
-                }
-            }
-            if(firstCase && secondCase){
+        int dp[201]={0};
+        for(auto i:nums){
+            dp[i]=i>=diff ? (dp[i-diff]+1) :(1);
+            if(dp[i]>=3){
                 cnt++;
-            }
+            }//index - value
+                //0 - 1  1 - 1  4 - 1+1(2) 6 -1  7 - 2+1(3) ....
         }
-        return cnt;
+        return cnt;   
     }
 };
