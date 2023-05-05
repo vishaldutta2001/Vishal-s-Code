9d190c8145183e81e44e4f4d1d55d2e0850e254a
Vishal Dutta <71959396+vishaldutta2001@users.noreply.github.com>
2023-05-05 17:23:33 +0530

    Time: 52 ms (85.48%), Space: 28.2 MB (54.12%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -4,7 +4,7 @@ public:
         int sum=0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i=i+2){
-            sum+=min(nums[i],nums[i+1]);
+            sum+=nums[i];
         }
         return sum;
     }
