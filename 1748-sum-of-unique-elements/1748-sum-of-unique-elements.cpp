335c892ce179a634fff65eda8a65f1dc8ecb512d
Vishal Dutta <71959396+vishaldutta2001@users.noreply.github.com>
2023-05-06 10:23:42 +0530

    Time: 6 ms (27.91%), Space: 7.8 MB (78.85%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -3,11 +3,8 @@ public:
     int sumOfUnique(vector<int>& nums) {
         int fre[101]={0};
         int sum=0;
-        for(int i=0;i<nums.size();i++){
-            fre[nums[i]]++;
-        }
-        for(int i=1;i<101;i++){
-            if(fre[i]==1) sum+=i;
+        for(int val:nums){
+            sum+= ++fre[val]==1 ? (val) : (fre[val]==2 ? -val : 0);
         }
         return sum;
     }
