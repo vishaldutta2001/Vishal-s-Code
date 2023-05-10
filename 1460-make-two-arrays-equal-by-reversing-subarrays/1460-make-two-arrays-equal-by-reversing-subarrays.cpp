3853131bb7580f37fe657614ba333911716194fc
Vishal Dutta <71959396+vishaldutta2001@users.noreply.github.com>
2023-05-10 13:01:14 +0530

    Time: 7 ms (97.06%), Space: 14.3 MB (91.79%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        if(target.size()!=arr.size()) return false;
+        int fre[1001]={0};
         for(int i=0;i<target.size();i++){
-            for(int j=i;j<arr.size();j++){
-                if(target[i]==arr[j]){
-                    swap(arr[j],arr[i]);
-                    break;
-                }
-            }
-        }   
-        for(int i=0;i<arr.size();i++){
-            if(target[i]!=arr[i]) {
-                return false;
-            }
+            fre[target[i]]++;
+            fre[arr[i]]--;
+        }
+        for(auto a:fre){
+            if(a!=0) return false;
         }
         return true;
     }
